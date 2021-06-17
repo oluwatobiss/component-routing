dfdf2095ba4ea95442a56418a30ba88c0068d928
Oluwatobi Sofela <oluwatobiss@codesweetly.com>
2021-06-17 23:15:05 +0100

    Change BrowserRouter to HashRouter

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import { HashRouter, Switch, Route, Link } from "react-router-dom";
 import App from "./App";
 import About from "./About";
 import Error from "./Error";
@@ -7,14 +7,14 @@ import Error from "./Error";
 function Routes() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <h1>Our Pages: <Link to="/">Home</Link> | <Link to="/about">About</Link></h1>
         <Switch>
           <Route exact path="/" component={App} />
           <Route exact path="/about" component={About} />
           <Route component={Error} />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
